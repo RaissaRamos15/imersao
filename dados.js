279056b252ca9e347e0cb38e977c2ca29bce467c
RaissaRamos15 <raissa.ramos150106@gmail.com>
2024-09-09 16:43:19 -0300

    imersão Alura

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade, a fada do ginásio, é uma das maiores ginastas brasileiras de todos os tempos. Medalhista olímpica e mundial, ela inspira milhões de jovens com sua dedicação e talento.",
+        instagram: "https://www.instagram.com/rebecarandrade/"
+      },
+      
+      {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza, a judoca brasileira, é conhecida por sua técnica impecável e sua força de vontade. Com diversas medalhas em competições internacionais, ela é um orgulho para o judô brasileiro.",
+        instagram: "https://www.instagram.com/biajudoca/"
+      },
+      {
+        titulo : "Rayssa Leal",
+        descricao :"Rayssa Leal, a fadinha, conquistou o mundo do skate com sua habilidade e carisma. Medalhista olímpica, ela é um ícone para a nova geração de skatistas e um símbolo da força e do talento da mulher no esporte.",
+        instagram : "https://www.instagram.com/rayssaleal/"
+    }
+];
